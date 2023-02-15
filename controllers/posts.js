fc993c7fcc0a7b7452d0bd96e8f55b12596bd260
coffeemug17 <mitulpandey2@gmail.com>
2023-02-14 18:26:38 -0800

    Saving all required attributes

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,32 +8,40 @@ module.exports = {
 };
 
 function index(req,res) {
-    res.render('posts/index', {title: 'All Posts'});
-}
-
-function newPost(req, res) {
-    const airlineId = req.params.id;
-    Post.findById(req.params.id)
-    .populate('airline')
-    .exec(function(err, post) {
-      Airline.findById(req.params.id,
-        function(err, airlines) {
-          console.log(airlines);
-          res.render(`posts/new`, {
-            title: 'Add a New Post',
-            post,
-            airlines
-          });
-        }
-      );
+    Post.find({}, function(err, posts) {
+        res.render('posts/index', {title: 'All Posts', posts});
     });
 }
 
+function newPost(req, res) {
+    Airline.findById(req.params.id, function(err, airline) {
+        res.render('posts/new', {title: 'Add a New Post', airline});
+    })
+}
+
 function create(req,res) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
     const post = new Post(req.body);
     post.save(function(err) {
+        console.log(err);
         if (err) return res.redirect('/posts/new');
         console.log(post);
         res.redirect('/posts');
     });
 }
+    // const airlineId = req.params.id;
+    // Post.findById(req.params.id)
+    // .populate('airline')
+    // .exec(function(err, post) {
+    //   Airline.findById(req.params.id,
+    //     function(err, airline) {
+    //       res.render(`posts/new`, {
+    //         title: 'Add a New Post',
+    //         post,
+    //         airline
+    //       });
+    //     }
+    //   );
+    // });
--- models/post.js
+++ models/post.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    airline: {
+    airlineId: {
         type: Schema.Types.ObjectId,
         ref: 'Airline',
         required: true
     },
-    airport: {
+    airlineName: {
+        type: String,
+        required: true
+    },
+    airlineIcon: {
         type: String,
         required: true
     },
@@ -15,6 +19,15 @@ const postSchema = new Schema({
         type: Number,
         min: 1,
         max: 5
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    legRoom: {
+        type: String,
+        enum: ['Extra Legroom', 'Regular Legroom', 'Less Legroom'],
+        default: 'Regular Legroom'
     },
     user: {
         type: Schema.Types.ObjectId,
